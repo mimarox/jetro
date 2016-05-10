fcb59b4ea48e64e97f62a4a171883f2f85cf3fce
oliver.burkhalter <oliver.burkhalter@unic.com>
2016-05-10 13:38:52 +0200

    Add more tests for issue #36

--- jetro-stream/src/test/java/net/sf/jetro/stream/visitor/JsonGeneratorTest.java
+++ jetro-stream/src/test/java/net/sf/jetro/stream/visitor/JsonGeneratorTest.java
@@ -32,4 +32,46 @@ public class JsonGeneratorTest {
 		// Then:
 		assertEquals(out.toString(), expectedJson);
 	}
+
+	@Test
+	public void testStringWithDouleQuotesInValueAtEnd() throws IOException {
+
+		// Given:
+		String valueIn = "Test msg \"double quotes\"";
+		String expectedJsonValue = "Test msg \\\"double quotes\\\"";
+		String expectedJson = "{\"key\":\"" + expectedJsonValue + "\"}";
+
+		// When:
+		StringBuffer out = new StringBuffer();
+		JsonGenerator testee = new JsonGenerator(out);
+
+		testee.beginObject();
+		testee.name("key");
+		testee.value(valueIn);
+		testee.endObject();
+
+		// Then:
+		assertEquals(out.toString(), expectedJson);
+	}
+
+	@Test
+	public void testStringWithDouleQuotesInValueAtBeginning() throws IOException {
+
+		// Given:
+		String valueIn = "\"double quotes\" testing";
+		String expectedJsonValue = "\\\"double quotes\\\" testing";
+		String expectedJson = "{\"key\":\"" + expectedJsonValue + "\"}";
+
+		// When:
+		StringBuffer out = new StringBuffer();
+		JsonGenerator testee = new JsonGenerator(out);
+
+		testee.beginObject();
+		testee.name("key");
+		testee.value(valueIn);
+		testee.endObject();
+
+		// Then:
+		assertEquals(out.toString(), expectedJson);
+	}
 }
