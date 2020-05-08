56baff4419e19cbcc8d7d9f74ff9191f429492c0
Matthias Rothe <matthias.rothe@gmx.com>
2020-05-08 16:47:44 +0200

    Added JavaDoc and some new convenience methods

--- jetro-transform/src/main/java/net/sf/jetro/transform/Jetro.java
+++ jetro-transform/src/main/java/net/sf/jetro/transform/Jetro.java
@@ -11,14 +11,46 @@ import net.sf.jetro.object.serializer.SerializationContext;
 import net.sf.jetro.object.visitor.ObjectVisitingReader;
 import net.sf.jetro.stream.JsonReader;
 import net.sf.jetro.stream.visitor.StreamVisitingReader;
+import net.sf.jetro.transform.highlevel.TransformationSpecification;
 import net.sf.jetro.tree.JsonType;
 import net.sf.jetro.tree.visitor.JsonElementVisitingReader;
 import net.sf.jetro.visitor.VisitingReader;
+import net.sf.jetro.visitor.chained.ChainedJsonVisitor;
 
+/**
+ * Main entry point to the transformation API.
+ * <p>
+ * Each transformation follows the pattern
+ * <code>Jetro.transform(source).applying(transformation).&lt;method of
+ * {@link TransformApplier}&gt;()</code>.
+ * <p>
+ * <code>source</code> can be any representation of JSON. See the individual
+ * <code>transform</code> methods for possible types.
+ * <p>
+ * <code>transformation</code> can either be an instance of any subtype of
+ * {@link ChainedJsonVisitor} or an instance of an implementation of
+ * {@link TransformationSpecification}. For details see there.
+ * <p>
+ * While the methods <code>transform</code> and <code>applying</code> collect
+ * the data for the transformation, the actual transformation only ever happens
+ * in the call to a method of the TransformApplier class.
+ * 
+ * @author Matthias Rothe
+ */
 public class Jetro {
 	private static final String SOURCE_NOT_NULL = "source must not be null";
 	private Jetro() {}
 	
+	/**
+	 * Transform JSON retrieved from the given {@link InputStream}. If the
+	 * InputStream yields anything but a valid JSON document, an exception
+	 * will be thrown during the transformation.
+	 * <p>
+	 * This method expects the JSON document to be encoded with UTF-8.
+	 * 
+	 * @param source the InputStream to retrieve the JSON document from
+	 * @return an instance of {@link TransformSourceCollector}
+	 */
 	public static TransformSourceCollector transform(final InputStream source) {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
 		
@@ -29,6 +61,16 @@ public class Jetro {
 		}
 	}
 	
+	/**
+	 * Transform JSON retrieved from the given {@link InputStream}. If the
+	 * InputStream yields anything but a valid JSON document, an exception
+	 * will be thrown during the transformation.
+	 * 
+	 * @param source the InputStream to retrieve the JSON document from
+	 * @param charsetName the name of the charset the JSON document is encoded with
+	 * @return an instance of {@link TransformSourceCollector}
+	 * @throws UnsupportedEncodingException if the named charset is not supported
+	 */
 	public static TransformSourceCollector transform(final InputStream source,
 			final String charsetName) throws UnsupportedEncodingException {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
@@ -37,11 +79,27 @@ public class Jetro {
 		return transform(new InputStreamReader(source, charsetName));
 	}
 	
+	/**
+	 * Transform JSON retrieved from the given {@link Reader}. If the
+	 * Reader yields anything but a valid JSON document, an exception
+	 * will be thrown during the transformation.
+	 * 
+	 * @param source the Reader to retrieve the JSON document from
+	 * @return an instance of {@link TransformSourceCollector}
+	 */
 	public static TransformSourceCollector transform(final Reader source) {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
 		return new TransformSourceCollector(getStreamVisitingReader(source));
 	}
 
+	/**
+	 * Transform JSON given as a {@link String}. If the given String contains
+	 * anything but a valid JSON document, an exception will be thrown during
+	 * the transformation.
+	 * 
+	 * @param source the JSON document
+	 * @return an instance of {@link TransformSourceCollector}
+	 */
 	public static TransformSourceCollector transform(final String source) {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
 		return new TransformSourceCollector(getStreamVisitingReader(
@@ -52,16 +110,41 @@ public class Jetro {
 		return new StreamVisitingReader(new JsonReader(source));
 	}
 	
+	/**
+	 * Transform the given {@link JsonType}.
+	 * 
+	 * @param source the JSON document
+	 * @return an instance of {@link TransformSourceCollector}
+	 */
 	public static TransformSourceCollector transform(final JsonType source) {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
 		return new TransformSourceCollector(new JsonElementVisitingReader(source));
 	}
 
+	/**
+	 * Serialize the given {@link Object} to JSON and transform it.
+	 * <p>
+	 * The source may be of any type the default {@link SerializationContext}
+	 * can serialize.
+	 * 
+	 * @param source the Object to serialize and transform
+	 * @return an instance of {@link TransformSourceCollector}
+	 */
 	public static TransformSourceCollector transform(final Object source) {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
 		return transform(source, new SerializationContext());
 	}
 	
+	/**
+	 * Serialize the given {@link Object} to JSON using the given
+	 * {@link SerializationContext} and transform it.
+	 * <p>
+	 * The source may be of any type the given {@link SerializationContext}
+	 * can serialize.
+	 * 
+	 * @param source the Object to serialize and transform
+	 * @return an instance of {@link TransformSourceCollector}
+	 */
 	public static TransformSourceCollector transform(final Object source,
 			final SerializationContext context) {
 		Objects.requireNonNull(source, SOURCE_NOT_NULL);
--- jetro-transform/src/main/java/net/sf/jetro/transform/TransformSourceCollector.java
+++ jetro-transform/src/main/java/net/sf/jetro/transform/TransformSourceCollector.java
@@ -6,7 +6,18 @@ import net.sf.jetro.transform.highlevel.TransformationSpecification;
 import net.sf.jetro.visitor.VisitingReader;
 import net.sf.jetro.visitor.chained.ChainedJsonVisitor;
 
+/**
+ * Part of the transformation API.
+ * <p>
+ * The methods of this class let you specify the transformation(s) to apply.
+ * 
+ * @author Matthias Rothe
+ * @see Jetro
+ */
 public class TransformSourceCollector {
+	private static final ChainedJsonVisitor<Void> NO_OP_TRANSFORMER =
+			new ChainedJsonVisitor<Void>() {};
+	
 	private final VisitingReader source;
 	
 	TransformSourceCollector(final VisitingReader source) {
@@ -14,13 +25,51 @@ public class TransformSourceCollector {
 		this.source = source;
 	}
 
+	/**
+	 * Specify a {@link ChainedJsonVisitor} to perform the intended transformation.
+	 * <p>
+	 * This transformer can be either a single instance of any subtype of
+	 * ChainedJsonVisitor or a chain of such instances composed by constructor injection
+	 * or by calling {@link ChainedJsonVisitor#attachVisitor(JsonVisitor)}.
+	 * <p>
+	 * Providing an instance of a subtype of ChainedJsonVisitor that doesn't override
+	 * any of its methods results in a no-op transformation.
+	 * 
+	 * @param transformer the ChainedJsonVisitor to perform the transformation
+	 * @return an instance of {@link TransformApplier}
+	 * @see #applyingNone()
+	 */
 	public <R> TransformApplier<R> applying(final ChainedJsonVisitor<R> transformer) {
 		Objects.requireNonNull(transformer, "transformer must not be null");
 		return new TransformApplier<>(source, transformer);
 	}
 
+	/**
+	 * Specify a {@link TransformationSpecification} to perform the intended
+	 * transformation.
+	 * <p>
+	 * Providing an instance of an implementation of TransformationSpecification
+	 * with an empty {@link TransformationSpecification#specify() specify()} method
+	 * will result in a no-op transformation.
+	 * 
+	 * @param specification the TransformationSpecification to perform the transformation
+	 * @return an instance of {@link TransformApplier}
+	 */
 	public TransformApplier<?> applying(final TransformationSpecification specification) {
 		Objects.requireNonNull(specification, "specification must not be null");
 		return applying(specification.toChainedJsonVisitor());
+	}
+	
+	/**
+	 * Use this method if you don't want any transformation to happen.
+	 * <p>
+	 * Not specifying any transformation might be useful if you use Jetro as a
+	 * bean mapper and the beans differ only in data types that are treated equally
+	 * in JSON, such as any number types and their wrappers or collection types.
+	 * 
+	 * @return an instance of {@link TransformApplier}
+	 */
+	public TransformApplier<?> applyingNone() {
+		return applying(NO_OP_TRANSFORMER);
 	}
 }
